676c1510df4c173c0d62e954775163602b8a65c3
Ammon Zerkle <ammonzerkle@gmail.com>
2020-11-11 16:43:21 -0700

    Passing Test FizzBuzz

--- TDDProjectTests/UnitTest1.cs
+++ TDDProjectTests/UnitTest1.cs
@@ -22,8 +22,16 @@ namespace TDDProjectTests
         public void TestMultiple5Buzz()
         {
             EulerProblem1 euler = new EulerProblem1();
-            string fizz = euler.GenerateFizzBuzz(5);
-            Assert.AreEqual("Buzz", fizz);
+            string buzz = euler.GenerateFizzBuzz(5);
+            Assert.AreEqual("Buzz", buzz);
+        }
+
+        [Test]
+        public void TestMultiple5and3FizzBuzz()
+        {
+            EulerProblem1 euler = new EulerProblem1();
+            string fizzbuzz = euler.GenerateFizzBuzz(15);
+            Assert.AreEqual("FizzBuzz", fizzbuzz);
         }
     }
 }
